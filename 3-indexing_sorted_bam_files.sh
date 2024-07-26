e4e58207475929b3b91813bafa80c66aae5190ec
ebaligand <162713203+ebaligand@users.noreply.github.com>
2024-07-26 11:56:13 +0200

    Update and rename Step 3 - Indexing and sorted BAM files to 3-indexing_sorted_bam_files.sh

--- 3-indexing_sorted_bam_files.sh
+++ 3-indexing_sorted_bam_files.sh
@@ -1,21 +1,20 @@
 ```bash
 # Find all BAM files in the result_dorado directory
-bam_file=$(find ./result_dorado/*.bam) # Path to BAM files after basecalling
+bam_files=$(find ./result_dorado -name "*.bam") # Path to BAM files after basecalling
 
 # Loop through each BAM file found
-for bam in $bam_file; do
+for bam in $bam_files; do
     # Get the basename of the BAM file, which is the patient ID
     id_patient=$(basename "$bam" .bam)
     # Define the sorted BAM file name
     sorted_bam="./result_dorado/${id_patient}_sorted.bam"
-    
+
     # Execute the singularity command to sort the BAM file
-    singularity exec --nv ./image_singularity/samtools.sif samtools sort -o "$sorted_bam" "$bam" 
-    
+    singularity exec --nv ./image_singularity/samtools.sif samtools sort -o "$sorted_bam" "$bam"
+
     # Execute the singularity command to index the sorted BAM file
     singularity exec --nv ./image_singularity/samtools.sif samtools index "$sorted_bam"
 done
 
-# Indexed and sorted files will be in the same dorado results folder
-
+echo "Sorting and indexing of all BAM files completed."
 ```
